b9033afe3e9dbd60d516f1f74d773035509dd2b5
malus91 <malinibhaskaran91@gmail.com>
2016-11-30 10:53:40 -0600

    Add to cart  Functionalities

--- history.php
+++ history.php
@@ -14,13 +14,22 @@ $user_id = $_SESSION['usr_id'];
 
 $res_row=array();
 
-$stmt=$db->prepare("SELECT DISTINCT property.propertyId,aptno,street,city,state,zipcode,sqft,bath,bhk,image,price FROM property,orderitems,orders WHERE orders.userid = (:userid) AND orderitems.orderid=orders.orderid AND property.isdeleted=0");
+//$stmt=$db->prepare("SELECT DISTINCT property.propertyId,aptno,street,city,state,zipcode,sqft,bath,bhk,image,price FROM property,orderitems,orders WHERE orders.userid = (:userid) AND orderitems.orderid=orders.orderid AND property.isdeleted=0");
+
+$stmt = $db->prepare("select property.propertyId,aptno,street,city,state,zipcode,sqft,bath,bhk,image,price,isdeleted FROM orders, orderitems,property WHERE orders.orderid = orderitems.orderid AND orders.userid = (:userid) AND property.propertyId = orderitems.propertyid");
+
+//$stmt=$db->prepare("SELECT * FROM property where propertyid in (Select orderitems.propertyId from order JOIN orderitems on orderitems.orderid=orders.orderid where orders.userid = (:userid)) AND property.isdeleted =0");
+                                  
 $stmt->bindParam(':userid', $user_id);
 $stmt->execute();
 
 while($row=$stmt->fetch())
 {
- $res=array("cn"=>$row['propertyId'],"a"=>$row['aptno'],"st"=>$row['street'],"c"=>$row['city'],"s"=>$row['state'],"z"=>$row['zipcode'],"sq"=>$row['sqft'],"b"=>$row['bath'],"bh"=>$row['bhk'],"i"=>$row['image'],"p"=>$row['price']);
+  if($row['isdeleted']==1)
+  $res=array("cn"=>$row['propertyId'],"a"=>$row['aptno'],"st"=>$row['street'],"c"=>$row['city'],"s"=>$row['state'],"z"=>$row['zipcode'],"sq"=>$row['sqft'],"b"=>$row['bath'],"bh"=>$row['bhk'],"i"=>$row['image'],"p"=>$row['price'],"d"=>"Not Available anymore");
+  else
+  $res=array("cn"=>$row['propertyId'],"a"=>$row['aptno'],"st"=>$row['street'],"c"=>$row['city'],"s"=>$row['state'],"z"=>$row['zipcode'],"sq"=>$row['sqft'],"b"=>$row['bath'],"bh"=>$row['bhk'],"i"=>$row['image'],"p"=>$row['price'],"d"=>"");  
+  
   $res_row[]=$res;
 }
 if(empty($res_row))
--- scripts/properties.js
+++ scripts/properties.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
 
 var username = $('#hdnSession').val(); 
+var isLoggedin = $('#userPresent').val();
 
 $.ajax({url: "properties.php",success: function(ds){
      
@@ -9,18 +10,31 @@ $.ajax({url: "properties.php",success: function(ds){
       localStorage.setItem('Allitems', ds);
       for(var i=0 ; i < obj.length;i++)
       {
-       
-     if(username == 0)
+	  if(isLoggedin)
+	  {
+	  if(username == 0)
       {
       $('#wpl').last().append('<li><div class="col-sm-4 col-lg-4 col-md-4"><div class="thumbnail"><img src= "img/'+ obj[i].i+'" alt="'+obj[i].cn+'"style="width:300px;height:180px;"><div class="caption"><h4 class="pull-right">'+obj[i].p+'$</h4><h4>'+obj[i].st+'</h4><p>'+obj[i].c+','+obj[i].s+','+obj[i].z+'</p><p>Area: '+obj[i].sq+' sqft</p><p>Bed: '+obj[i].bh+'  Bath: '+obj[i].b+'</p></div><div class="ratings"><button id="AddCart" name ="'+obj[i].cn+'"">Add To WishList</button></div></div></li>');
       $("ul.pagination3").quickPagination({pagerLocation:"bottom",pageSize:"3"});
       }
       else
       {
-      $('#wpl').last().append('<li><div class="col-sm-4 col-lg-4 col-md-4"><div class="thumbnail"><img src= "img/'+ obj[i].i+'" alt="'+obj[i].cn+'"style="width:300px;height:180px;"><div class="caption"><h4 class="pull-right">'+obj[i].p+'$</h4><h4>'+obj[i].st+'</h4><p>'+obj[i].c+','+obj[i].s+','+obj[i].z+'</p><p>Area: '+obj[i].sq+' sqft</p><p>Bed: '+obj[i].bh+'  Bath: '+obj[i].b+'</p></div><div class="ratings"><button id="AddCart" name ="'+obj[i].cn+'"">Add To WishList</button><button id="Edit" name ="'+obj[i].cn+'"">Edit</button></div></div></li>');
+      $('#wpl').last().append('<li><div class="col-sm-4 col-lg-4 col-md-4"><div class="thumbnail"><img src= "img/'+ obj[i].i+'" alt="'+obj[i].cn+'"style="width:300px;height:180px;"><div class="caption"><h4 class="pull-right">'+obj[i].p+'$</h4><h4>'+obj[i].st+'</h4><p>'+obj[i].c+','+obj[i].s+','+obj[i].z+'</p><p>Area: '+obj[i].sq+' sqft</p><p>Bed: '+obj[i].bh+'  Bath: '+obj[i].b+'</p></div><div class="ratings"><button id="Edit" name ="'+obj[i].cn+'"">Edit</button></div></div></li>');
+     
+	  $("ul.pagination3").quickPagination({pagerLocation:"bottom",pageSize:"3"});
+	  } 
+	  }
+	  else
+	  {
+		
+		$('#wpl').last().append('<li><div class="col-sm-4 col-lg-4 col-md-4"><div class="thumbnail"><img src= "img/'+ obj[i].i+'" alt="'+obj[i].cn+'"style="width:300px;height:180px;"><div class="caption"><h4 class="pull-right">'+obj[i].p+'$</h4><h4>'+obj[i].st+'</h4><p>'+obj[i].c+','+obj[i].s+','+obj[i].z+'</p><p>Area: '+obj[i].sq+' sqft</p><p>Bed: '+obj[i].bh+'  Bath: '+obj[i].b+'</p></div></div></li>');
       $("ul.pagination3").quickPagination({pagerLocation:"bottom",pageSize:"3"});
+
+		
+	  }
+      
       }
-      }
+	  
     }
 
 });
